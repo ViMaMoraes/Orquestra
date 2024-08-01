f702db40197d4b7ba813b2402586beb85e46316a
mvmoraes81 <mvmoraes81@gmail.com>
2024-08-01 13:08:01 +0000

    Atualizando

--- src/app/letras/emJesus/page.tsx
+++ src/app/letras/emJesus/page.tsx
@@ -4,9 +4,8 @@ export default function emJesus() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Só em Jesus&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+        <p className="font-mono font-bold fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          SÓ EM JESUS
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -28,84 +27,70 @@ export default function emJesus() {
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        Só em Jesus posso esperar\n
+        É minha força é minha luz \n
+        Ele é a pedra angular \n
+        Que nada pode abalar \n\n
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        Que imenso amor profunda paz \n
+        Quando a batalha terminar \n
+        Jesus é tudo para mim\n
+        Dá-me abrigo até o fim.\n\n
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        Em carne o Verbo se tornou\n
+        Deus encarnado num bebê \n
+        É dom de amor e retidão \n
+        Salvou a quem o desprezou\n\n
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        Ali na cruz em que morreu \n
+        De Deus o plano se cumpriu\n
+        O meu pecado carregou\n
+        Por sua morte vivo estou.\n\n
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        Seu corpo inerte ali no chão \n
+        É luz em meio a escuridão \n
+        Da sepultura levantou\n
+        Glorioso dia se tornou.\n\n
+
+        Quando em Vitória ressurgiu\n 
+        Os pecadores redimiu\n
+        Agora é meu e dEle sou\n
+        Pois com seu sangue me comprou.\n\n
+
+        A minha fé é o meu amor \n
+        Estão firmados no Senhor \n
+        Estão firmados no Senhor.\n\n
+
+        A minha fé é o meu amor\n
+        Estão firmados no Senhor \n
+        Estão firmados no Senhor\n\n
+
+        A morte eu não vou temer \n
+        Pois tenho o seu poder\n
+        Desde o nascer até o morrer\n
+        Cristo comanda o meu viver.\n\n
+
+        O inferno não conseguirá\n
+        De Suas mãos me arrancar \n
+        Até voltar ou me chamar\n
+        Em seu poder me manterei.\n\n
+
+        Este é o meu respirar \n
+        Este é o meu respirar\n
+        Teu Santo Espírito \n
+        Vive em mim.\n\n
+
+        E este é o meu Pão \n
+        E este é o meu Pão \n
+        Tua vontade \n
+        Feita em mim. \n
+
+        E eu, eu nada sou sem Ti\n
+        E eu, perdido estou sem Ti.\n
+
+        E eu, eu nada sou sem Ti\n
+        E eu, perdido estou sem Ti\n 
+        <h1>Jesus!<\h1>
       </div>
     </main>
   );
